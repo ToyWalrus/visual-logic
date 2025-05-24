f6fb4f2cca3c01979935c7bc723029a5fde419a3
Trevor Howell <tkhowell95@gmail.com>
2025-05-23 19:39:26 -0600

    Style the canvas & toggles a bit

--- src/components/MainCanvas.vue
+++ src/components/MainCanvas.vue
@@ -3,21 +3,27 @@
 		<slot name="header" />
 		<div class="canvas-content">
 			<div class="left-side">
-				<ToggleCircle
-					v-for="(control, index) in inputs"
-					:key="`left-${index}`"
-					:control="control"
-					@toggle="$emit('toggle', control)"
-				/>
+				<div class="io-background">
+					<ToggleCircle
+						v-for="(control, index) in inputs"
+						:key="`left-${index}`"
+						:control="control"
+						@toggle="$emit('toggle', control)"
+						size="XL"
+					/>
+				</div>
 			</div>
 			<div class="main-area" />
 			<div class="right-side">
-				<ToggleCircle
-					v-for="(control, index) in outputs"
-					:key="`right-${index}`"
-					:control="control"
-					@toggle="$emit('toggle', control)"
-				/>
+				<div class="io-background">
+					<ToggleCircle
+						v-for="(control, index) in inputs"
+						:key="`right-${index}`"
+						:control="control"
+						@toggle="$emit('toggle', control)"
+						size="XL"
+					/>
+				</div>
 			</div>
 		</div>
 		<slot name="footer" />
@@ -40,11 +46,24 @@ defineEmits<{
 
 <style scoped>
 .canvas {
+	--background-color-rgb: 254, 254, 254;
+	--background-color: rgb(var(--background-color-rgb));
+	--border-color: rgb(212, 212, 212);
+}
+
+@media (prefers-color-scheme: dark) {
+	.canvas {
+		--border-color: rgb(60, 60, 60);
+		--background-color-rgb: 30, 30, 30;
+	}
+}
+
+.canvas {
 	display: flex;
 	flex-direction: column;
 	width: 100%;
 	height: 100vh;
-	background-color: #fefefe;
+	background-color: var(--background-color);
 }
 
 .canvas-content {
@@ -53,20 +72,45 @@ defineEmits<{
 	flex: 1;
 }
 
-.left-side,
-.right-side {
-	width: 80px;
+.io-background {
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-	gap: 40px;
-	padding: 20px;
+	align-items: center;
+	gap: 30px;
+
+	width: 80px;
+	background: radial-gradient(
+		circle,
+		rgba(var(--background-color-rgb), 1) 75%,
+		rgba(var(--background-color-rgb), 0) 100%
+	);
+	padding: 36px 0;
+}
+
+.left-side,
+.right-side {
+	position: absolute;
+	top: 0;
+	bottom: 0;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+}
+
+.left-side {
+	left: 0;
+}
+
+.right-side {
+	right: 0;
 }
 
 .main-area {
 	flex: 1;
-	border: 1px solid #d4d4d4;
-	margin: 20px 0;
+	border: 1px solid var(--border-color);
+	margin: 20px 40px;
 	border-radius: 4px;
 }
 </style>
--- src/components/ToggleCircle.vue
+++ src/components/ToggleCircle.vue
@@ -2,21 +2,39 @@
 	<div class="circle-container">
 		<div
 			class="circle"
-			:class="{ active: control.active, toggleable: control.toggleable }"
+			:class="[sizeClass, { active: control.active, toggleable: control.toggleable }]"
 			@click="$emit('toggle')"
-		></div>
+		/>
 		<div v-if="control.label" class="label">{{ control.label }}</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 import type { IOControl } from '@/types';
+import { computed } from 'vue';
 
-defineProps<{
+const props = defineProps<{
 	control: IOControl;
+	size?: 'XS' | 'S' | 'M' | 'L' | 'XL';
 }>();
 
 defineEmits(['toggle']);
+
+const sizeClass = computed(() => {
+	switch (props.size) {
+		case 'XS':
+			return 'circle-xs';
+		case 'S':
+			return 'circle-s';
+		case 'L':
+			return 'circle-l';
+		case 'XL':
+			return 'circle-xl';
+		case 'M':
+		default:
+			return 'circle-m';
+	}
+});
 </script>
 
 <style scoped>
@@ -27,6 +45,7 @@ defineEmits(['toggle']);
 }
 
 .circle-container {
+	position: relative;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -34,8 +53,6 @@ defineEmits(['toggle']);
 }
 
 .circle {
-	width: 24px;
-	height: 24px;
 	border-radius: 50%;
 	background-color: var(--background-color);
 	transition: background-color 200ms ease;
@@ -59,5 +76,52 @@ defineEmits(['toggle']);
 	background-color: var(--inactive-color);
 	border-radius: 4px;
 	color: #fff;
+	display: block;
+	position: absolute;
+	z-index: 1;
+	white-space: nowrap;
+	opacity: 0;
+	pointer-events: none;
+	transition: opacity 200ms ease;
+}
+
+.circle-xs {
+	width: 16px;
+	height: 16px;
+}
+.circle-s {
+	width: 24px;
+	height: 24px;
+}
+.circle-m {
+	width: 32px;
+	height: 32px;
+}
+.circle-l {
+	width: 40px;
+	height: 40px;
+}
+.circle-xl {
+	width: 48px;
+	height: 48px;
+}
+
+.circle-xs + .label,
+.circle-s + .label {
+	margin-top: 28px;
+}
+.circle-m + .label {
+	margin-top: 36px;
+}
+.circle-l + .label {
+	margin-top: 44px;
+}
+.circle-xl + .label {
+	margin-top: 52px;
+}
+
+.circle-container:hover .label {
+	opacity: 1;
+	pointer-events: auto;
 }
 </style>
